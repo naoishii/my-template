ba8ebd11dae948e36f3d736be323221c5e48cfaa
naoishii <naoishii.dev@gmail.com>
2017-05-05 03:22:57 +0900

    middleware

--- src/store.js
+++ src/store.js
@@ -1,7 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 
 import reducer from './modules';
 
-const store = createStore(reducer);
+let middleware = [thunkMiddleware];
+if (process.env.NODE_ENV !== 'production') {
+  const loggerMiddleware = require('redux-logger').default; // eslint-disable-line
+  middleware = [...middleware, loggerMiddleware];
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware),
+);
 
 export default store;
